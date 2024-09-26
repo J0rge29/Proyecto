9f53f6aa3784548ba6dedea375215cb57427e297
MSI 15 <jorgeisaac390603@gmail.com>
2024-09-26 13:34:44 -0600

    Start

---
+++ rpg/rpg/entities/Game.java
@@ -0,0 +1,39 @@
+package rpg.entities;
+
+import javax.swing.*;
+
+public class Game {
+    /**
+     * Nombres de los pesonajes
+     */
+    private Player player;
+    private Enemy enemy;
+
+    public Game() {
+        this.enemy = new Enemy("Goblin");
+        this.player = new Player("Hero");
+    }
+
+    public void startGame() {
+        /**
+         *	Funcion que inicia el juego
+         */
+        while (player.isAlive() && enemy.isAlive()) {
+            player.attack(enemy);
+            if (enemy.isAlive()) {
+                enemy.attack(player);
+            }
+        }
+        /**
+         *	Funcion que se ejecuta mientras alguno de los personajes muera
+         */
+        if (player.isAlive()) {
+            JOptionPane.showMessageDialog(null, "Derrotaste al anemigo");
+        } else {
+            JOptionPane.showMessageDialog(null, "Fin del juego");
+        }
+    }
+}
+
+
+
---
+++ rpg/rpg/entities/Stats.java
@@ -0,0 +1,18 @@
+package rpg.entities;
+
+public enum Stats {
+
+    HP,
+    MAX_HP,
+    MP,
+    MAX_MP,
+    ATTACK,
+    DEFENSE,
+    SPEED,
+    DEXTERITY,
+    LUCK,
+    ACCURACY,
+    EVASION,
+    CRITICAL_HIT,
+    CRITICAL_DAMAGE;
+}
