1bb849cefd73f04a2501da119e2bd6ad23414688
Luis Hernandez <l22241829@toluca.tecnm.mx>
2024-10-12 00:39:04 -0600

    Update

--- src/rpg/items/ShortSword.java
+++ src/rpg/items/ShortSword.java
@@ -11,7 +11,7 @@ public class ShortSword extends Equipment {
     @Override
     protected void initItem() {
         this.name = "Espada Corta";
-        this.description = "Una espada basica. Ideal para ataques rapidos.";
+        this.description = "Espada basica. Ideal para ataques rapidos.";
         this.price = 75;
         this.type = ItemType.WEAPON;
         this.stats = new HashMap<>();
